3597a255748f7f0ce8aa552706fc346ae45bafcc
Lauren Birts <birtslauren@gmail.com>
2020-03-30 23:13:07 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -9,8 +9,8 @@ def join_nested_strings(src)
       if src[row_index][element_index].class == string
         string _array << src[row_index][element_index]
       end
+      element_index += 1
     end
-    element_index += 1
+    row_index += 1
   end
-  row_index += 1
 end
